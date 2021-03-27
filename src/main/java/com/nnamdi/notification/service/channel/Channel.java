f68b41463462b32383d6fcd0df618437e2959d19
Nnamdi <nnwabuokei@paga.com>
2021-03-27 11:05:07 +0100

    Service class Channel that specifies the methods for notification and ChannelFactory that manages the various channel type implementation

---
+++ src/main/java/com/nnamdi/notification/service/channel/Channel.java
@@ -0,0 +1,16 @@
+package com.nnamdi.notification.service.channel;
+
+import com.nnamdi.notification.model.ChannelType;
+import com.nnamdi.notification.model.Message;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Channel {
+    default  void notify(Message message) {
+        throw new RuntimeException("Notify method is not implemented yet");
+    }
+
+    default boolean supports(ChannelType type) {
+        throw  new RuntimeException("Notify method is not implemented ywt ");
+    }
+}
---
+++ src/main/java/com/nnamdi/notification/service/channel/ChannelFactory.java
@@ -0,0 +1,41 @@
+package com.nnamdi.notification.service.channel;
+
+import com.nnamdi.notification.model.ChannelType;
+import com.nnamdi.notification.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ChannelFactory {
+    private final List<Channel> channelList;
+
+    @Autowired
+    public ChannelFactory(List<Channel> channelList) {
+        this.channelList = channelList;
+    }
+
+
+    public List<Channel> getChannelList() {
+        return channelList;
+    }
+
+    public Channel get(ChannelType channelType) {
+        return channelList.stream()
+                .filter(service -> service.supports(channelType))
+                .findFirst()
+                .orElseThrow(() ->new RuntimeException("No Channel found with type " + channelType));
+    }
+
+    public void notifyAll(Message message) {
+        for (Channel channel: channelList){
+            channel.notify(message);
+        }
+    }
+
+    public  List<Channel> getChannels() {
+        return channelList;
+    }
+
+}
