1737625e5d16c725d9179a61fb2b3478518533d6
Nnamdi <nnwabuokei@paga.com>
2021-03-27 13:20:41 +0100

    Modified the test class adding @ContextConfiguration

---
+++ src/test/java/com/nnamdi/notification/NotificationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nnamdi.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = {NotificationApplication.class})
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
