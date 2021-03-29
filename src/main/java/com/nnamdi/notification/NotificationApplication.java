e2a2758942fbdc0daab4f927215d3d10b2e300da
Nnamdi <nnwabuokei@paga.com>
2021-03-29 14:55:37 +0100

    Removed zombie code

--- src/main/java/com/nnamdi/notification/NotificationApplication.java
+++ src/main/java/com/nnamdi/notification/NotificationApplication.java
@@ -2,12 +2,13 @@ package com.nnamdi.notification;
 
 import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 @EnableEmailTools
 public class NotificationApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationApplication.class, args);
