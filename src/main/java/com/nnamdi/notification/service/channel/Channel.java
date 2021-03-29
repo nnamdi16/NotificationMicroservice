0e758afee333ab953265d6a2381f2ed2c5a9a97d
Nnamdi <nnwabuokei@paga.com>
2021-03-29 14:40:15 +0100

    Added  MessagingException to notify method in Channel interface

--- src/main/java/com/nnamdi/notification/service/channel/Channel.java
+++ src/main/java/com/nnamdi/notification/service/channel/Channel.java
@@ -4,9 +4,11 @@ import com.nnamdi.notification.model.ChannelType;
 import com.nnamdi.notification.model.Message;
 import org.springframework.stereotype.Repository;
 
+import javax.mail.MessagingException;
+
 @Repository
 public interface Channel {
-    default  void notify(Message message) {
+    default  void notify(Message message) throws MessagingException {
         throw new RuntimeException("Notify method is not implemented yet");
     }
 
